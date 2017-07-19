f2e2d07b7290518bf75049d3abfc3ad91c5046c8
benli11 <Benli11@users.noreply.github.com>
2017-07-18 18:38:08 -0700

    tests for ID and CUR

--- tests/tests.py
+++ tests/tests.py
@@ -154,6 +154,148 @@ class test_mf(TestCase):
         percent_error = 100 * np.linalg.norm(A - Ak) / np.linalg.norm(A)
         
         assert percent_error < atol_float64 
+        
+        
+#
+#******************************************************************************
+#
+class test_cur(TestCase):
+    def setUp(self):
+        np.random.seed(123)
+
+    def test_id_col(self):
+        m, k = 100, 10
+        A = np.array(np.random.randn(m, k), np.float64)
+        A = A.dot(A.T)
+        A = A[:,0:50]
+        
+        # index_set = False
+        C, V = interp_decomp(A, k=k+2, mode='column', index_set=False)
+        relative_error = (np.linalg.norm(A - C.dot(V)) / np.linalg.norm(A))
+        assert relative_error < 1e-4  
+
+        # index_set = True
+        C, V = interp_decomp(A, k=k+2, mode='column', index_set=True)
+        relative_error = (np.linalg.norm(A - A[:,C].dot(V)) / np.linalg.norm(A))
+        assert relative_error < 1e-4  
+
+    def test_id_row(self):
+        m, k = 100, 10
+        A = np.array(np.random.randn(m, k), np.float64)
+        A = A.dot(A.T)
+        A = A[:,0:50]
+        
+        # index_set = False
+        Z, R = interp_decomp(A, k=k+2, mode='row', index_set=False)
+        relative_error = (np.linalg.norm(A - Z.dot(R)) / np.linalg.norm(A))
+        assert relative_error < 1e-4  
+
+        # index_set = True
+        Z, R = interp_decomp(A, k=k+2, mode='row', index_set=True)
+        relative_error = (np.linalg.norm(A - Z.dot(A[R,:])) / np.linalg.norm(A))
+        assert relative_error < 1e-4 
+     
+
+    def test_rid_col(self):
+        m, k = 100, 10
+        A = np.array(np.random.randn(m, k), np.float64)
+        A = A.dot(A.T)
+        A = A[:,0:50]
+        
+        # index_set = False
+        C, V = rinterp_decomp(A, k=k+2, mode='column', index_set=False)
+        relative_error = (np.linalg.norm(A - C.dot(V)) / np.linalg.norm(A))
+        assert relative_error < 1e-4  
+
+        # index_set = True
+        C, V = rinterp_decomp(A, k=k+2, mode='column', index_set=True)
+        relative_error = (np.linalg.norm(A - A[:,C].dot(V)) / np.linalg.norm(A))
+        assert relative_error < 1e-4  
+
+    def test_rid_row(self):
+        m, k = 100, 10
+        A = np.array(np.random.randn(m, k), np.float64)
+        A = A.dot(A.T)
+        A = A[:,0:50]
+        
+        # index_set = False
+        Z, R = rinterp_decomp(A, k=k+2, mode='row', index_set=False)
+        relative_error = (np.linalg.norm(A - Z.dot(R)) / np.linalg.norm(A))
+        assert relative_error < 1e-4  
+
+        # index_set = True
+        Z, R = rinterp_decomp(A, k=k+2, mode='row', index_set=True)
+        relative_error = (np.linalg.norm(A - Z.dot(A[R,:])) / np.linalg.norm(A))
+        assert relative_error < 1e-4        
+
+    def test_ridqb_col(self):
+        m, k = 100, 10
+        A = np.array(np.random.randn(m, k), np.float64)
+        A = A.dot(A.T)
+        A = A[:,0:50]
+        
+        # index_set = False
+        C, V = rinterp_decomp_qb(A, k=k+2, mode='column', index_set=False)
+        relative_error = (np.linalg.norm(A - C.dot(V)) / np.linalg.norm(A))
+        assert relative_error < 1e-4  
+
+        # index_set = True
+        C, V = rinterp_decomp_qb(A, k=k+2, mode='column', index_set=True)
+        relative_error = (np.linalg.norm(A - A[:,C].dot(V)) / np.linalg.norm(A))
+        assert relative_error < 1e-4  
+
+
+    def test_ridqb_row(self):
+        m, k = 100, 10
+        A = np.array(np.random.randn(m, k), np.float64)
+        A = A.dot(A.T)
+        A = A[:,0:50]
+        
+        # index_set = False
+        Z, R = rinterp_decomp_qb(A, k=k+2, mode='row', index_set=False)
+        relative_error = (np.linalg.norm(A - Z.dot(R)) / np.linalg.norm(A))
+        assert relative_error < 1e-4  
+
+        # index_set = True
+        Z, R = rinterp_decomp_qb(A, k=k+2, mode='row', index_set=True)
+        relative_error = (np.linalg.norm(A - Z.dot(A[R,:])) / np.linalg.norm(A))
+        assert relative_error < 1e-4  
+        
+        
+    def test_cur(self):
+        m, k = 100, 10
+        A = np.array(np.random.randn(m, k), np.float64)
+        A = A.dot(A.T)
+        A = A[:,0:50]
+        
+        # index_set = False
+        C, U, R = cur(A, k=k+2, index_set=False)
+        relative_error = (np.linalg.norm(A - C.dot(U).dot(R)) / np.linalg.norm(A))
+        assert relative_error < 1e-4  
+
+        # index_set = True
+        C, U, R = cur(A, k=k+2, index_set=True)
+        relative_error = (np.linalg.norm(A - A[:,C].dot(U).dot(A[R,:])) / np.linalg.norm(A))
+        assert relative_error < 1e-4        
+
+
+    def test_cur(self):
+        m, k = 100, 10
+        A = np.array(np.random.randn(m, k), np.float64)
+        A = A.dot(A.T)
+        A = A[:,0:50]
+        
+        # index_set = False
+        C, U, R = rcur(A, k=k+2, index_set=False)
+        relative_error = (np.linalg.norm(A - C.dot(U).dot(R)) / np.linalg.norm(A))
+        assert relative_error < 1e-4  
+
+        # index_set = True
+        C, U, R = rcur(A, k=k+2, index_set=True)
+        relative_error = (np.linalg.norm(A - A[:,C].dot(U).dot(A[R,:])) / np.linalg.norm(A))
+        assert relative_error < 1e-4 
+
+                
 #
 #******************************************************************************
 #
