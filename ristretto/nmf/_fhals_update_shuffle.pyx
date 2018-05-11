b5104e949b305c5712fa25c2e2462381b33455e7
Joseph Knox <jknox13@uw.edu>
2018-05-11 00:30:13 -0700

    refactor nmf module for better PIP standards

--- ristretto/nmf/_fhals_update_shuffle.pyx
+++ ristretto/nmf/_fhals_update_shuffle.pyx
@@ -22,7 +22,6 @@ def _fhals_update_shuffle(double[:, ::1] W, double[:, :] HHt, double[:, :] XHt,
             t = permutation[s]
 
             for i in range(n_samples):
-
                 gradient = -XHt[i, t]
 
                 for r in range(n_components):
@@ -31,17 +30,16 @@ def _fhals_update_shuffle(double[:, ::1] W, double[:, :] HHt, double[:, :] XHt,
                 # projected gradient
                 if W[i, t] == 0.0:
                     projected_gradient = min(0.0, gradient)
-                    
-                elif W[i, t] > 0.0:   
+
+                elif W[i, t] > 0.0:
                     projected_gradient = gradient
-                    
+
                 else:
                     projected_gradient = 0.0
 
-                violation += fabs(projected_gradient)   
-
+                violation += fabs(projected_gradient)
 
                 if HHt[t, t] != 0:
                     W[i, t] = max(0.0, W[i, t] - gradient / HHt[t, t])
-                
+
     return violation
